e7b6fcccde45f294fd36465ecc81d7f5845ab347
DEEPAK-SINGH-BVM <deepaksingh.bvminfotech@gmail.com>
2025-07-07 17:56:39 +0530

    Test-2
    CRUD with Filter (Gender ,Language , Country)

--- Day-17-Test2/script.js
+++ Day-17-Test2/script.js
@@ -1,32 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let form = document.getElementById("form-list");
-  let firstName = document.getElementById("firstName");
-  let lastName = document.getElementById("lastName");
+  let form = document.getElementById("form-Data");
+  // console.log(form);
+  
+  let firstName = document.getElementById("first-Name");
+  let lastName = document.getElementById("last-Name");
   let rollNo = document.getElementById("rollNo");
-  let dateOfBirth = document.getElementById("dob");
-  let selectedCountry = document.getElementById("country");
+  let dateOfBirth = document.getElementById("DateOfBirth");
+  let selectedCountry = document.getElementById("select-country");
   let imageFile = document.getElementById("input-file");
 
-  let list = document.getElementById("student-list");
+  let list = document.getElementById("student-data");
 
   //Error
-
   let error_name = document.getElementById("error_name");
   let error_last = document.getElementById("error_last");
   let error_roll = document.getElementById("error_roll");
-  let error_dob = document.getElementById("error_dob");
-  let error_Gender = document.getElementById("error_Gender");
+  let error_date = document.getElementById("error_date");
+  let error_gender = document.getElementById("error_gender");
   let error_country = document.getElementById("error_country");
   let error_language = document.getElementById("error_language");
-
-  // Shorting
 
   let imageData = [];
   let selectedRow = null;
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-
     let fName = firstName.value;
     let lName = lastName.value;
     let roll = rollNo.value;
@@ -37,54 +35,47 @@ document.addEventListener("DOMContentLoaded", function () {
       form.querySelectorAll(`input[name="language"]:checked`)
     ).map((input) => input.value);
 
-    // Error
     error_name.textContent = "";
     error_last.textContent = "";
     error_roll.textContent = "";
-    error_dob.textContent = "";
-    error_Gender.textContent = "";
+    error_date.textContent = "";
+    error_gender.textContent = "";
     error_country.textContent = "";
     error_language.textContent = "";
 
     let isValid = true;
-    if (!fName) {
-      error_name.textContent = "First Name Required";
-      isValid = false;
+    if (!fName) {error_name.textContent = "First Name Required";isValid = false;
+      console.log('hello1');
     }
-    if (!lName) {
-      error_last.textContent = "Last Name Required";
-      isValid = false;
+    if (!lName) {error_last.textContent = "Last Name Required";isValid = false;
+      console.log('hello2');
     }
-    if (!roll) {
-      error_roll.textContent = "Roll Number Required";
-      isValid = false;
+    if (!roll) {error_roll.textContent = "Roll Number Required";isValid = false;
+      console.log('hello3');
     }
-    if (!dob) {
-      error_dob.textContent = "Date of Birth Required";
-      isValid = false;
+    if (!dob) {error_date.textContent = "Date of Birth Required";isValid = false;
+      console.log('hello4');
     }
-    if (!gender) {
-      error_Gender.textContent = "Gender Required";
-      isValid = false;
+    if (!gender) {error_gender.textContent = "Gender Required";isValid = false;
+      console.log('hello5');
     }
-    if (!country) {
-      error_country.textContent = "Country Required";
-      isValid = false;
+    if (!country) {error_country.textContent = "Country Required";isValid = false;
+      console.log('hello6');
     }
-    if (language == 0) {
-      error_language.textContent = "At List 1 Language Required";
-      isValid = false;
+    if (language == 0) {error_language.textContent = "At List 1 Language Required";isValid = false;
+      console.log('hello7');
     }
-    if (!isValid) return;
+    if (!isValid) return
+    console.log(!isValid);
 
-    let imageHTMLDate = imageData.map(
+    let imageHTML = imageData.map(
       (src) => `<img src="${src}" height="50px" width="50px">`
     );
 
     if (selectedRow === null) {
       let row = document.createElement("tr");
       row.innerHTML = `
-            <td>${imageHTMLDate}</td>
+            <td>${imageHTML}</td>
             <td>${fName}</td>
             <td>${lName}</td>
             <td>${roll}</td>
@@ -93,13 +84,12 @@ document.addEventListener("DOMContentLoaded", function () {
             <td>${country}</td>
             <td>${language}</td>
              <td>
-                <button class="edit">Edit</button>
-                <button class="delete">Delete</button>
+                
            </td>     
         `;
       list.append(row);
     } else {
-      selectedRow.children[0].innerHTML = imageHTMLDate;
+      selectedRow.children[0].innerHTML = imageHTML;
       selectedRow.children[1].innerHTML = fName;
       selectedRow.children[2].innerHTML = lName;
       selectedRow.children[3].innerHTML = roll;
@@ -111,7 +101,6 @@ document.addEventListener("DOMContentLoaded", function () {
     form.reset();
     selectedRow = null;
 
-    // Sorting
   });
 
   list.addEventListener("click", function (e) {
@@ -124,11 +113,9 @@ document.addEventListener("DOMContentLoaded", function () {
       ).map((img) => img.src);
 
       firstName.value = selectedRow.children[1].textContent;
-      console.log(firstName.value);
-
       lastName.value = selectedRow.children[2].textContent;
       rollNo.value = selectedRow.children[3].textContent;
-      dob.value = selectedRow.children[4].textContent;
+      dateOfBirth.value = selectedRow.children[4].textContent;
       const genderValue = selectedRow.children[5].textContent;
 
       const genderInput = form.querySelector(
@@ -136,26 +123,38 @@ document.addEventListener("DOMContentLoaded", function () {
       );
       if (genderInput) genderInput.checked = true;
 
-      country.value = selectedRow.children[6].textContent;
+      selectedCountry.value = selectedRow.children[6].textContent;
 
-      const language = selectedRow.children[7].textContent
-        .split(",")
-        .map((l) => l);
+      const language = selectedRow.children[7].textContent.split(",").map((l) => l);
       form.querySelectorAll('input[name="language"]').forEach((input) => {
         input.checked = language.includes(input.value);
       });
     }
-
-    if (e.target.classList.contains("delete")) {
-      e.target.closest("tr").remove();
-      form.reset();
-      selectedRow = null;
-      imageData = [];
-    }
+    
+    // if (e.target.classList.contains("delete")) {
+    //   e.target.closest("tr").remove();
+    //   form.reset();
+    //   selectedRow = null;
+    //   imageData = [];
+    // }
+    // function deleteData(index){
+    //   list.splice(index,1).remove()
+    // } deleteData()
+    // function deleteData(index){
+    //   if(Array.isArray(list) && index>=0 && index < list.length){
+    //     list.splice(index,1)
+        
+    //   }
+    //   else{
+    //     console.log('invalid Index');
+    //     alert("invalid Index !!")
+    //   }
+    // }
+   
   });
 
   imageFile.addEventListener("change", function () {
-    // console.log('working');
+    
     imageData = [];
     const files = [...imageFile.files];
 
@@ -167,18 +166,14 @@ document.addEventListener("DOMContentLoaded", function () {
       reader.readAsDataURL(file);
     });
   });
-  // Filter Logic
+  let Gender = document.getElementById("filter-gender");
+  let Country = document.getElementById("filter-country");
+  let Language = document.getElementById("filter-language");
 
-  let filterGender = document.getElementById("filter-gender");
-  console.log(filterGender);
-  let filterCountry = document.getElementById("filter-country");
-  let filterLanguage = document.getElementById("filter-language");
-
-  function applyFilter() {
-    let genderValue = filterGender.value;
-    console.log(genderValue);
-    let countryValue = filterCountry.value;
-    let languageValue = filterLanguage.value;
+  function Filter() {
+    let genderValue = Gender.value;
+    let countryValue = Country.value;
+    let languageValue = Language.value;
 
     const rows = list.querySelectorAll("tr");
 
@@ -200,8 +195,9 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-  filterGender.addEventListener("change", applyFilter);
-  filterCountry.addEventListener("change", applyFilter);
-  filterLanguage.addEventListener("change", applyFilter);
+  Gender.addEventListener("change", Filter);
+  Country.addEventListener("change", Filter);
+  Language.addEventListener("change", Filter);
+  // console.log('running');
   
 });
